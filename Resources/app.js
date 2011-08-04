4c236a8c442410bec98fab1df054df2b2e843766
Andre Mougeot <mougeot.andre@gmail.com>
2011-08-03 21:30:59 -0600

    Fixed problem with displaying the character names Cleaning up generated framework files

--- Resources/app.js
+++ Resources/app.js
@@ -1,22 +1,27 @@
+Ti.include('utils.js');
+
 // Set default background
 Titanium.UI.setBackgroundColor('#333');
 
 var tabGroup = Titanium.UI.createTabGroup();
 
-Ti.include('utils.js');
+var characterCardLabels = [createLabel("Jenny LeClerc"),
+							createLabel("Darin \"Flash\" Williams"),
+							createLabel("Vivan Lopez"),
+							createLabel("Peter Akimoto"),
+							createLabel("Little Zoe Ingstrom"),
+							createLabel("Professor Longfellow"),
+							createLabel("Madame Zostra")];
+
+var cardScrollView = createScrollableView(characterCardLabels);
 
 var cardsWindow = createWindow('Cards');
-cardsWindow.add(createLabel('Choose your card'));
-
+cardsWindow.add(cardScrollView);
 var cardsTab = createTab('Cards', cardsWindow);
 
-var selectedCardWindow = createWindow('Selected');
-selectedCardWindow.add(createLabel('This is your selected card'));
-
-var selectedCardTab = createTab('Selected', selectedCardWindow);
-
+var selectedCardWindow = createWindow('Selected Card');
+var selectedCardTab = createTab('Selected Card', selectedCardWindow);
 
 tabGroup.addTab(cardsTab);
 tabGroup.addTab(selectedCardTab);
-
 tabGroup.open();
--- Resources/utils.js
+++ Resources/utils.js
@@ -15,10 +15,22 @@ function createTab(tabName, innerWindow) {
 
 function createLabel(labelText) {
 	return Ti.UI.createLabel({
-		color:'#aaa',
 		text: labelText,
-		font:{fontSize:18,fontFamily:'Helvetica Neue'},
-		textAlign:'left',
-		width:'auto'
+		height:'auto',
+	    width:'auto',
+	    shadowColor:'#aaa',
+	    shadowOffset:{x:5,y:5},
+	    color:'#aaa',
+	    font:{fontSize:48},
+	    textAlign:'center'
+	});
+}
+
+function createScrollableView(viewArray) {
+	return Ti.UI.createScrollableView({
+		views:viewArray,
+		showPagingControl:true,
+		pagingControlHeight:10,
+		maxZoomScale:2.0
 	});
 }
